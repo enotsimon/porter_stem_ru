59f6c651db4c8021cf8b39c2d887a60ecc6372cb
Ivan Skorobogatko <skorobogatko@superjob.ru>
2014-10-17 10:24:23 +0400

    add license

--- src/porter_stem_ru_tests.erl
+++ src/porter_stem_ru_tests.erl
@@ -1,4 +1,14 @@
 %% -*- coding: utf-8 -*-
+
+%
+% ----------------------------------------------------------------------------
+% "THE BEER-WARE LICENSE" (Revision 42):
+% skorobogatko@github.com wrote this file. As long as you retain this notice you
+% can do whatever you want with this stuff. If we meet some day, and you think
+% this stuff is worth it, you can buy me a beer in return
+% ----------------------------------------------------------------------------
+%
+
 -module(porter_stem_ru_tests).
 -include_lib("eunit/include/eunit.hrl").
 
